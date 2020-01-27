e7cd13ba07b3ce59da181f56cdf9156519469acb
ratoz <herlandro.1999@students.amikom.ac.id>
2020-01-27 20:22:14 +0700

    Change on Navbar (Ando)

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,7 +1,37 @@
-<div class="container-fluid d-md-none bg-primary">
-        Testing Smartphone with portrait view
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav mr-auto">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Arsip</a>
+            <a class="nav-item nav-link" href="#">Profil</a>
+        </div>
+        <div class="navbar-nav mr-5">
+            <a class="nav-item nav-link" href="#">Login</a>
+        </div>
+    </div>
+</nav>
+
+<!--<div class="container-fluid d-md-none bg-primary">
+    Testing Smartphone with portrait view
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Testing Web</h2>
+        </div>
+    </div>
 </div>
 
-<div class="container-fluid d-none d-md-block bg-secondary text-white">
-        Testing PC, Tablet, Smartphone with landscape view
-</div>
+<div class="container-fluid d-none d-md-block bg-success text-white">
+    Testing PC, Tablet, Smartphone with landscape view
+    <div class="row">
+        <div class="col-4">
+            <h2>Testing Web</h2>
+        </div>
+        <div class="col-8">
+
+        </div>
+    </div>
+</div>-->
