61ddc6125412849198a99dce799d3f82661d5f7f
akbarHm <harimukti789@gmail.com>
2020-02-03 10:24:31 +0700

    Update Form Surat

---
+++ application/views/form_surat/index.php
@@ -0,0 +1,132 @@
+<form id="acf-form" class="acf-form" action="" method="post">
+        
+        <div id="acf-form-data" class="acf-hidden">
+            <input id="_acf_screen" name="_acf_screen" value="acf_form" type="hidden">
+            <input id="_acf_post_id" name="_acf_post_id" value="new_post" type="hidden">
+            <input id="_acf_validation" name="_acf_validation" value="1" type="hidden">
+            <input id="_acf_form" name="_acf_form" value="..." type="hidden">
+            <input id="_acf_nonce" name="_acf_nonce" value="6aa82f255b" type="hidden">
+            <input id="_acf_changed" name="_acf_changed" value="0" type="hidden">
+        </div>
+        
+        <div class="acf-fields acf-form-fields -top">
+        
+            <!-- .row -->
+            <div class="row">
+                
+                <!-- .form-group .col-12 -->
+                <div 
+                    class="acf-field acf-field-text acf-field-5ccc199a22617 is-required form-group col-12" 
+                    data-name="name" 
+                    data-type="text" 
+                    data-key="field_5ccc199a22617" 
+                    data-required="1">
+                    
+                    <div class="acf-label">
+                        <label for="acf-field_5ccc199a22617">
+                        
+                            <!-- .text-danger -->
+                            Name <span class="acf-required text-danger">*</span>
+                            
+                        </label>
+                    </div>
+                    
+                    <div class="acf-input">
+                        <div class="acf-input-wrap">
+                        
+                            <!-- .form-control -->
+                            <input type="text" id="acf-field_5ccc199a22617" class="form-control" name="acf[field_5ccc199a22617]" required="required">
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                <!-- .form-group .col-12 -->
+                <div 
+                    class="acf-field acf-field-email acf-field-5ccc19a022618 is-required form-group col-12" 
+                    data-name="email" 
+                    data-type="email" 
+                    data-key="field_5ccc19a022618" 
+                    data-required="1">
+                    
+                    <div class="acf-label">
+                        <label for="acf-field_5ccc19a022618">
+                        
+                            <!-- .text-danger -->
+                            E-Mail <span class="acf-required text-danger">*</span>
+                            
+                        </label>
+                    </div>
+                    
+                    <div class="acf-input">
+                        <div class="acf-input-wrap">
+                        
+                            <!-- .form-control -->
+                            <input type="email" id="acf-field_5ccc19a022618" class="form-control" name="acf[field_5ccc19a022618]" required="required">
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                <!-- .form-group .col-12 -->
+                <div 
+                    class="acf-field acf-field-textarea acf-field-5ccc19a922619 is-required form-group col-12" 
+                    data-name="text" 
+                    data-type="textarea" 
+                    data-key="field_5ccc19a922619" 
+                    data-required="1">
+                    
+                    <div class="acf-label">
+                        <label for="acf-field_5ccc19a922619">
+                            
+                            <!-- .text-danger -->
+                            Text <span class="acf-required text-danger">*</span>
+                            
+                        </label>
+                    </div>
+                    
+                    <div class="acf-input">
+                    
+                        <!-- .form-control -->
+                        <textarea id="acf-field_5ccc19a922619" class="form-control" name="acf[field_5ccc19a922619]" rows="8" required="required"></textarea>
+                        
+                    </div>
+                </div>
+                
+                <div 
+                    class="acf-field acf-field-text acf-field--validate-email form-group col-12" 
+                    style="display:none !important;" 
+                    data-name="_validate_email" 
+                    data-type="text" 
+                    data-key="_validate_email">
+                    
+                    <div class="acf-label">
+                        <label for="acf-_validate_email">
+                            Validate Email
+                        </label>
+                    </div>
+                    
+                    <div class="acf-input">
+                        <div class="acf-input-wrap">
+                            <input type="text" id="acf-_validate_email" class="form-control" name="acf[_validate_email]">
+                        </div>
+                    </div>
+                    
+                </div>
+             
+            <!-- /.row -->
+            </div>
+            
+        </div>
+        
+        <!-- Submit -->
+        <div class="acf-form-submit">
+            
+            <!-- .btn .btn-primary -->
+            <input type="submit" class="acf-button button button-primary button-large btn btn-primary" value="Submit"> 
+            <span class="acf-spinner"></span>
+            
+        </div>
+    </form>
