c9b992af5157b4b927b94cf31afd50bf805d3d08
akbarHm <harimukti789@gmail.com>
2020-02-04 14:53:30 +0700

    Update dashboard (Image card) & navbar icon for side bar

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -6,6 +6,15 @@
   <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
 </div>
 
+<div class="card" style="width:400px">
+    <img class="card-img-top" src="<?=PATHIMG?>Sleman.png" alt="Logo Sleman" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">John Doe</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary stretched-link">See Profile</a>
+    </div>
+  </div>
+
 <div class="container">
   <p>This is some text.</p>      
   <p>This is another text.</p>      
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,11 +1,10 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-freespeechblue">
-    <button type="button" id="sidetoggle" class="btn btn-warning">Menu</button>
+    <i id="sidetoggle" class="fas fa-bars" style="color: #d2dae2;"></i>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav mr-auto">
-            <a><img src="<?=PATHIMG?>Sleman.png"></a>
             <a class="nav-item nav-link" href="<?=base_url('Home')?>">Home</a>
             <a class="nav-item nav-link" href="<?=base_url('Form_Surat')?>">Arsip</a>
             <a class="nav-item nav-link" href="#">Profil</a>
