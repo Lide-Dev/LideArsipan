4739de983e32c22f0022a87fe04149145e366dd1
akbarHm <harimukti789@gmail.com>
2020-02-07 11:14:52 +0700

    Update Form Regsitrasi Surat

--- application/views/form_surat/index.php
+++ application/views/form_surat/index.php
@@ -1,60 +1,46 @@
 <div class="container">
-<form class="needs-validation" novalidate>
-  <div class="form-row">
-    <div class="col-md-4 mb-3">
-      <label for="validationTooltip01">First name</label>
-      <input type="text" class="form-control" id="validationTooltip01" placeholder="First name" value="Mark" required>
-      <div class="valid-tooltip">
-        Looks good!
-      </div>
-    </div>
-    <div class="col-md-4 mb-3">
-      <label for="validationTooltip02">Last name</label>
-      <input type="text" class="form-control" id="validationTooltip02" placeholder="Last name" value="Otto" required>
-      <div class="valid-tooltip">
-        Looks good!
-      </div>
-    </div>
-    <div class="col-md-4 mb-3">
-      <label for="validationTooltipUsername">Username</label>
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
-        </div>
-        <input type="text" class="form-control" id="validationTooltipUsername" placeholder="Username" aria-describedby="validationTooltipUsernamePrepend" required>
-        <div class="invalid-tooltip">
-          Please choose a unique and valid username.
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="form-row">
-    <div class="col-md-6 mb-3">
-      <label for="validationTooltip03">City</label>
-      <input type="text" class="form-control" id="validationTooltip03" placeholder="City" required>
-      <div class="invalid-tooltip">
-        Please provide a valid city.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationTooltip04">State</label>
-      <input type="text" class="form-control" id="validationTooltip04" placeholder="State" required>
-      <div class="invalid-tooltip">
-        Please provide a valid state.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationTooltip05">Zip</label>
-      <input type="text" class="form-control" id="validationTooltip05" placeholder="Zip" required>
-      <div class="invalid-tooltip">
-        Please provide a valid zip.
-      </div>
-    </div>
-  </div>
-  <div class="custom-file">
-  <input type="file" class="custom-file-input" id="customFile">
-  <label class="custom-file-label" for="customFile">Choose file</label>
+<div style="text-align: center;" class="mt-3 mb-3">
+<h2>REGISTRASI SURAT</h2>
 </div>
-  <button class="btn btn-primary mt-3 mb-3" type="submit">Submit form</button>
+<div class="container pt-2" style="background-color: #808e9b; border-radius: 5px;">
+  <form>
+  <div class="form-row">
+  <div class="form-group col-md-2">
+      <label for="inputZip">Kode 1</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Kode 2</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Kode 3</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Kode 4</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+</form>
+</div><!-- End of form surat -->
+
+<form>
+	<div class="form-group">
+		<label for="nama">Asal Surat</label>
+		<input type="text" id="nama" class="form-control" placeholder="">
+	</div>
+
+	<div class="form-group">
+		<label for="umur">Isi Ringkas</label>
+		<textarea type="number" id="umur" class="form-control" placeholder=""></textarea>
+	</div>
+
+	<div class="form-group">
+		<label for="alamat">Keterangan</label>
+		<textarea class="form-control" id="alamat" rows="3" placeholder=""></textarea>
+	</div>
+
+	<button type="submit" class="btn btn-primary mb-3">Submit</button>
 </form>
 </div>
