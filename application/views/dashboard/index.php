f1ffe556886473cab323f7682cd53169efe6a77e
akbarHm <harimukti789@gmail.com>
2020-02-07 11:20:55 +0700

    Update Info about dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -2,22 +2,22 @@
 <body>
 
 <div class="jumbotron">
-  <h1>Bootstrap Tutorial</h1>      
-  <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+  <h1>PEMERINTAH DESA CONDONGCATUR</h1>      
+  <p>Pemerintah Desa Condongcatur berdiri pada tanggal 26 Desember 1946 berdasarkan Maklumat Pemerintah Daerah Istimewa Yogyakarta Nomor 5 Tahun 1948.</p>
 </div>
 
 <div class="card" style="width:400px">
     <img class="card-img-top" src="<?=PATHIMG?>Sleman.png" alt="Logo Sleman" style="width:100%">
     <div class="card-body">
       <h4 class="card-title">John Doe</h4>
-      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-      <a href="#" class="btn btn-primary stretched-link">See Profile</a>
+      <p class="card-text">...</p>
+      <a href="#" class="btn btn-primary stretched-link">Lihat Profile</a>
     </div>
   </div>
 
 <div class="container">
-  <p>This is some text.</p>      
-  <p>This is another text.</p>      
+  <p>...</p>      
+  <p>...</p>      
 </div>
 </body>
 </div>
