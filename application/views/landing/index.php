2b3808d4193b9170fdb69ea613732f7bb9fb6662
akbarHm <harimukti789@gmail.com>
2020-03-22 09:58:19 +0700

    Footer New Landing Page

--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -94,7 +94,7 @@
         </ul>
     </nav>
 
-    <div class="container-fluid" style="height:1000px">
+    <div class="container-fluid">
         <section>
             <div class="container">
                 <!--Main layout-->
@@ -221,6 +221,111 @@
                 </section>
             </div>
     </div>
+
+       <!-- Footer -->
+   <footer class="page-footer font-small pt-4" style="background-color: #1e272e;">
+
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
+
+  <!-- Footer links -->
+  <div class="row text-center text-md-left mt-3 pb-3">
+
+    <!-- Grid column -->
+    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3" style="color: #808e9b;">
+      <h6 class="text-uppercase mb-4 font-weight-bold">PEMERINTAH DESA CONDONGCATUR</h6>
+      <p>Pemerintah Desa Condongcatur berdiri pada tanggal 26 Desember 1946 berdasarkan Maklumat Pemerintah Daerah Istimewa Yogyakarta Nomor 5 Tahun 1948.</p>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="w-100 clearfix d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3" style="color: #808e9b;">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+      <p>
+        <a href="#!">LideDev</a>
+      </p>
+    </div>
+
+    <!-- Grid column -->
+    <hr class="w-100 clearfix d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-4 col-lg-3 col-xl-7 mx-auto mt-3" style="color: #808e9b;">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+      <p>
+        <i class="fas fa-home mr-3"></i> Depok, Sleman, Yogyakarta, 55583</p>
+      <p>
+        <i class="fas fa-envelope mr-3"></i> condongcatur1946@gmail.com</p>
+      <p>
+        <i class="fas fa-print mr-3"></i> (0274) 885 689</p>
+      <p class="float-right"><a href="#">Back to top</a></p>
+    </div>
+
+    <!-- Grid column -->
+
+  </div>
+  <!-- Footer links -->
+
+  <hr>
+
+  <!-- Grid row -->
+  <div class="row d-flex align-items-center">
+
+    <!-- Grid column -->
+    <div class="col-md-7 col-lg-8">
+
+      <!--Copyright-->
+      <p class="text-center text-md-left" style="color: white;">© 2020 Copyright:
+        <a href="#">
+          <strong> LideDev</strong>
+        </a>
+      </p>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-md-5 col-lg-4 ml-lg-0">
+
+      <!-- Social buttons -->
+      <div class="text-center text-md-right">
+        <ul class="list-unstyled list-inline">
+          <li class="list-inline-item">
+            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+              <i class="fab fa-google-plus-g"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-sm rgba-white-slight mx-1">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
 </body>
 
 </html>
