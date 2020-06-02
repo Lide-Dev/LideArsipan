9b0252971addd0bbc6c797a6b95b1bece58dce0f
akbarHm <harimukti789@gmail.com>
2020-06-02 09:28:15 +0700

    fixing navbar mobile

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -18,23 +18,18 @@
         </div>
         <div class="navbar-nav mr-md-5">
             <div class=" d-md-none">
-                <ul class="">
-                    <li class="sidebar-brand mb-2">
-                        <a style="display: none;" href="#">
-                            Menu
-                        </a>
-                    </li>
-                    <li>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
                         <a href="<?= base_url('dashboard') ?>"><i class="fas fa-home"></i><span style="margin-left: 10px;">Dashboard</span></a>
                     </li>
-                    <li>
+                    <li class="list-group-item">
                         <a href="<?= base_url('registrasi_surat') ?>"><i class="fas fa-save"></i><span style="margin-left: 12.7px;">Registrasi Surat</span></a>
                     </li>
-                    <li>
+                    <li class="list-group-item">
                         <a href="<?= base_url('Arsip') ?>"><i class="fas fa-search"></i><span style="margin-left: 11.5px;">Cari Arsip</span></a>
                     </li>
                     <?php if (!empty($accadmin) && $accadmin === 'admin') { ?>
-                        <li>
+                        <li class="list-group-item">
                             <a href="<?= base_url('admin/dashboard') ?>"><i class="fas fa-users-cog"></i><span style="margin-left: 9px;">Dashboard Admin</span></a>
                         </li>
                     <?php  } ?>
