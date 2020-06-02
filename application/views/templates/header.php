a9702b000d65401e2c47f2ea2434e4f1ec279255
ratoz <herlandro.1999@students.amikom.ac.id>
2020-06-02 08:33:25 +0700

    fix ui at all page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,43 +20,45 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
     <link rel="icon" type="image/png" sizes="32x32" href=<?= base_url('assets/img/favicon-32x32.png') ?>>
 
-    <?php if(empty($landing)||(!empty($landing)&&!$landing)){?>
-    <link rel="stylesheet" href=<?= base_url('assets/css/bootstrap.min.css') ?>>
-    <link rel="stylesheet" href=<?= base_url('assets/css/all.min.css') ?>>
-    <link href="<?= base_url('assets/css/fontawesome.min.css') ?>" rel="stylesheet">
-    <link rel="stylesheet" href=<?= base_url('assets/css/lidearsip.css') ?>>
-    <link rel="stylesheet" href=<?= base_url('assets/css/sidebar.css') ?>>
-    <link rel="stylesheet" href=<?= base_url('assets/css/jquery-ui.min.css') ?>>
+    <?php if (empty($landing) || (!empty($landing) && !$landing)) { ?>
+        <link rel="stylesheet" href=<?= base_url('assets/css/bootstrap.min.css') ?>>
+        <link rel="stylesheet" href=<?= base_url('assets/css/all.min.css') ?>>
+        <link href="<?= base_url('assets/css/fontawesome.min.css') ?>" rel="stylesheet">
+        <link rel="stylesheet" href=<?= base_url('assets/css/lidearsip.css') ?>>
+        <?php if (empty($admin) || !$admin) { ?>
+            <link rel="stylesheet" href=<?= base_url('assets/css/sidebar.css') ?>>
+        <?php } ?>
+        <link rel="stylesheet" href=<?= base_url('assets/css/jquery-ui.min.css') ?>>
     <?php } else { ?>
-    <link rel="stylesheet" href=<?= base_url('assets/css/all.min.css') ?>>
-    <link href="<?= base_url('assets/css/fontawesome.min.css') ?>" rel="stylesheet">
-    <link rel="stylesheet" href=<?= base_url('assets/css/bootstrap.min.css') ?>>
-    <link rel="icon" type="image/png" sizes="32x32" href=<?= base_url('assets/img/favicon-32x32.png') ?>>
-    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href=<?= base_url('assets/css/animation_landing.css') ?>>
+        <link rel="stylesheet" href=<?= base_url('assets/css/all.min.css') ?>>
+        <link href="<?= base_url('assets/css/fontawesome.min.css') ?>" rel="stylesheet">
+        <link rel="stylesheet" href=<?= base_url('assets/css/bootstrap.min.css') ?>>
+        <link rel="icon" type="image/png" sizes="32x32" href=<?= base_url('assets/img/favicon-32x32.png') ?>>
+        <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href=<?= base_url('assets/css/animation_landing.css') ?>>
     <?php } ?>
-<!--For CSS Pages-->
-    <?php if (!empty($page)&&$page==="login"){ ?>
-    <link rel="stylesheet" href=<?= base_url('assets/css/login_css.css') ?>>
-    <link rel="stylesheet" href=<?= base_url('assets/css/bug_report.css') ?>>
-    <link rel="stylesheet" href=<?= base_url('assets/css/lidearsip.css') ?>>
-    <?php }?>
-    <?php if (!empty($page)&&$page==="form_surat"){ ?>
-    <link rel="stylesheet" href=<?= base_url('assets/css/ceklis_form_surat.css') ?>>
-    <?php }?>
-    <?php if (!empty($page)&&$page==="dashboard"){ ?>
-    <link rel="stylesheet" href=<?= base_url('assets/css/tutorial_dashboard.css') ?>>
-    <link rel="stylesheet" href=<?= base_url('assets/css/jumbotron_dashboard.css') ?>>
-    <link rel="stylesheet" href=<?= base_url('assets/css/page/style.css') ?>>
-    <?php }?>
-    <?php if (!empty($page)&&$page==="arsip"){ ?>
-    <link rel="stylesheet" href=<?= base_url('assets/css/button_table.css') ?>>
-    <?php }?>
-<!--End CSS Pages-->
+    <!--For CSS Pages-->
+    <?php if (!empty($page) && $page === "login") { ?>
+        <link rel="stylesheet" href=<?= base_url('assets/css/login_css.css') ?>>
+        <link rel="stylesheet" href=<?= base_url('assets/css/bug_report.css') ?>>
+        <link rel="stylesheet" href=<?= base_url('assets/css/lidearsip.css') ?>>
+    <?php } ?>
+    <?php if (!empty($page) && $page === "form_surat") { ?>
+        <link rel="stylesheet" href=<?= base_url('assets/css/ceklis_form_surat.css') ?>>
+    <?php } ?>
+    <?php if (!empty($page) && $page === "dashboard") { ?>
+        <link rel="stylesheet" href=<?= base_url('assets/css/tutorial_dashboard.css') ?>>
+        <link rel="stylesheet" href=<?= base_url('assets/css/jumbotron_dashboard.css') ?>>
+        <link rel="stylesheet" href=<?= base_url('assets/css/page/style.css') ?>>
+    <?php } ?>
+    <?php if (!empty($page) && $page === "arsip") { ?>
+        <link rel="stylesheet" href=<?= base_url('assets/css/button_table.css') ?>>
+    <?php } ?>
+    <!--End CSS Pages-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/b-1.6.1/cr-1.5.2/fc-3.3.0/fh-3.1.6/kt-2.5.1/r-2.2.3/rg-1.1.1/rr-1.2.6/sc-2.0.1/sp-1.0.1/sl-1.3.1/datatables.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Roboto&display=swap" rel="stylesheet">
-<!--For Font awesome only-->
-    
+    <!--For Font awesome only-->
+
     <script src="https://code.highcharts.com/highcharts.js"></script>
 
 
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,22 +1,49 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-blackpearl">
-    <?php if ($sidebar){?>
-        <i class='glyphicon glyphicon-play whiteText' data-toggle='tooltip' data-placement='top' title='MENU' aria-hidden='true'><i id="sidetoggle" class="nav-item fas fa-bars" style="color: #d2dae2;"></i></i>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <!-- span class="navbar-toggler-icon"></span -->
-        <i class="nav-item fas fa-angle-double-down" style="color: #d2dae2;"></i>
-    </button>
+    <?php if ($sidebar) { ?>
+        <div class="d-none d-md-block">
+            <i class='glyphicon glyphicon-play whiteText' data-toggle='tooltip' data-placement='top' title='MENU' aria-hidden='true'><i id="sidetoggle" class="nav-item fas fa-bars" style="color: #d2dae2;"></i></i>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+
+            <!-- span class="navbar-toggler-icon"></span -->
+            <i class="nav-item fas fa-angle-double-down" style="color: #d2dae2;"></i>
+        </button>
+
     <?php } ?>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ml-3 mr-auto">
-        <a class="navbar-brand d-none d-md-block" href="<?= base_url('dashboard') ?>">
-          <img src=<?=base_url(PATHIMG.'Logo_Navbar.svg')?> alt="Sleman">
-        </a>
+            <a class="navbar-brand d-none d-md-block" href="<?= base_url('dashboard') ?>">
+                <img src=<?= base_url(PATHIMG . 'Logo_Navbar.svg') ?> alt="Sleman">
+            </a>
         </div>
         <div class="navbar-nav mr-md-5">
+            <div class=" d-md-none">
+                <ul class="">
+                    <li class="sidebar-brand mb-2">
+                        <a style="display: none;" href="#">
+                            Menu
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url('dashboard') ?>"><i class="fas fa-home"></i><span style="margin-left: 10px;">Dashboard</span></a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url('registrasi_surat') ?>"><i class="fas fa-save"></i><span style="margin-left: 12.7px;">Registrasi Surat</span></a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url('Arsip') ?>"><i class="fas fa-search"></i><span style="margin-left: 11.5px;">Cari Arsip</span></a>
+                    </li>
+                    <?php if (!empty($accadmin) && $accadmin === 'admin') { ?>
+                        <li>
+                            <a href="<?= base_url('admin/dashboard') ?>"><i class="fas fa-users-cog"></i><span style="margin-left: 9px;">Dashboard Admin</span></a>
+                        </li>
+                    <?php  } ?>
+                </ul>
+            </div>
             <?=
-            empty($_SESSION['idlogin'])
-            ? "<a class='nav-item nav-link' href='".base_url('Login')."'><i class='glyphicon glyphicon-play whiteText' data-toggle='tooltip' data-placement='top' title='MASUK' aria-hidden='true'><i class='fas fa-sign-in-alt'></i></i></a>"
-            : "<a class='nav-item nav-link' href='".base_url('go/logout')."'><i class='glyphicon glyphicon-play whiteText' data-toggle='tooltip' data-placement='top' title='KELUAR' aria-hidden='true'><i class='fas fa-sign-out-alt white'></i></i></a>";
+                empty($_SESSION['idlogin'])
+                    ? "<a class='nav-item nav-link' href='" . base_url('Login') . "'><i class='glyphicon glyphicon-play whiteText' data-toggle='tooltip' data-placement='top' title='MASUK' aria-hidden='true'><i class='fas fa-sign-in-alt'></i></i><span class='ml-2 d-md-none'>Login</span></a>"
+                    : "<a class='nav-item nav-link' href='" . base_url('go/logout') . "'><i class='glyphicon glyphicon-play whiteText' data-toggle='tooltip' data-placement='top' title='KELUAR' aria-hidden='true'><i class='fas fa-sign-out-alt white'></i></i></i><span class='ml-2 d-md-none'>Logout</span></a>";
             ?>
         </div>
     </div>
