8eb55a36d31a490c61cbecfe8d5a0fcd88349863
ratoz <herlandro.1999@students.amikom.ac.id>
2020-06-11 13:02:21 +0700

    Activate CSRF Token for security

--- application/config/config.php
+++ application/config/config.php
@@ -449,12 +449,13 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$base=$config['base_url'];
+$config['csrf_protection'] = TRUE;
+$config['csrf_token_name'] = 'vldt';
+$config['csrf_cookie_name'] = 'vldt_cookie';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = array("registrasi-surat/kode/patch");
 
 /*
 |--------------------------------------------------------------------------
